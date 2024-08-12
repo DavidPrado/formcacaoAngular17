ae910eea76f23ab608a3c883af6f9a2e341681e2
David Stefani Prado <daviprado1995@gmail.com>
2024-08-12 14:38:11 -0300

    enviar projeto ao git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,32 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `<div style="display: flex; justify-content: center; align-items: center; flex-direction: column; gap: 1rem;">
+  @for (user of userDatasList; track user.id) {
+    @switch (user.age) {
+      @case (20) {
+        <span>{{user.name}} is {{user.profession}} and is 20 years old</span>
+      }
+      @default {
+        <span>{{user.name}} is {{user.profession}} and yuor age is {{user.age}}</span>
+      }
+    }
+  } @empty {
+    <span>Dont have datas to show</span>
+  }
+</div>`,
 })
 export class AppComponent {
   title = 'learnAngular17';
+  userDatasList: Array<{
+    name: string;
+    age: number;
+    profession: string;
+    id: string;
+  }> = [
+    { age: 20, name: 'Marcos', profession: 'Software Developer', id: '123' },
+    { age: 30, name: 'Marcelo', profession: 'Software Developer', id: '456' },
+    { age: 40, name: 'Carlos', profession: 'Scrum Master', id: '789' },
+    { age: 30, name: 'Maria', profession: 'UX Designer', id: '123' },
+  ];
 }
